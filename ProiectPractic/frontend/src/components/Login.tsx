20f4cde6555bb3d6fd50730abb22bdebc255b3a9
dorogus1 <marogeldragosflorinel@gmail.com>
2025-03-21 07:33:50 +0200

    Am adaugat css la Login Page

--- ProiectPractic/frontend/src/components/Login.tsx
+++ ProiectPractic/frontend/src/components/Login.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "./styles.css";
 
 const Login = () => {
     const [username, setUsername] = useState("");
@@ -19,22 +20,26 @@ const Login = () => {
             alert("Autentificare eșuată!");
         }
     };
+
     const goToRegister = () => {
         navigate("/register");
     };
+
     return (
-        <div>
+        <div className="auth-container">
             <h2>Login</h2>
-            <h2>Le vreau le vreau</h2>
-            <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <div className="input-group">
+                <label>Username</label>
+                <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+            </div>
+            <div className="input-group">
+                <label>Password</label>
+                <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            </div>
             <button onClick={handleLogin}>Login</button>
             <button onClick={goToRegister}>Register</button>
-
         </div>
     );
 };
-
-
 
 export default Login;
